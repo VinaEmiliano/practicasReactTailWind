78d6183ef8362c8ffa8c11fb5eb53a0243632e48
Emiliano Viña <emisk805@gmail.com>
2023-01-20 19:30:26 +0100

    Actualizacion Actualizacion de los estilos de la caja de comentarios

--- PrimerTailwind/src/components/VentanaModal.jsx
+++ PrimerTailwind/src/components/VentanaModal.jsx
@@ -27,10 +27,10 @@ const VentanaModal = ({modal, setModal, guardarComentarios}) => {
   }
 
   return (
-    <section className="flex flex-col bg-black w-full ">
+    <section className="flex flex-col bg-slate-800 opacity-90 w-4/5 mx-auto rounded-3xl ">
       
-      <div className="w-10 h-10 ">
-        <img src="../../public/cerrar.svg" alt="boton cerrar" className=" " onClick={handleCloseModal} />
+      <div className=" m-5 w-10 h-10 ">
+        <img src="../../public/cerrar.svg" alt="boton cerrar" className="" onClick={handleCloseModal} />
       </div>
       
       <form action="" 
@@ -38,24 +38,26 @@ const VentanaModal = ({modal, setModal, guardarComentarios}) => {
         onSubmit={handleSubmit}
         >
         
-        <legend>Deja tu comentario</legend>
+        <legend className="text-4xl font-semibold">Deja tu comentario</legend>
         
         {mensaje && <Mensaje>{mensaje}</Mensaje>}
         
-        <label htmlFor="email" className="p-4 m-4">Ingresa tu email</label>
+        <label htmlFor="email" className="text-2xl font-semibold p-4 m-4">Ingresa tu email</label>
         <input 
           id="email"
           type="email" 
+          placeholder="email"
           className="p-4 m-4 text-black"
           value={email}
           onChange ={ e => setEmail(e.target.value)}
         />
         
-        <label htmlFor="textArea" className="p-4 m-4">Deja tu comentario</label>
+        <label htmlFor="textArea" className="text-2xl font-semibold p-4 m-4">Deja tu comentario</label>
         <textarea 
           name="textArea" 
           id="textArea" 
-          cols="30" rows="10" 
+          cols="30" rows="10"
+          placeholder="Aqui va tu comentario" 
           className="p-4 m-4 text-black"
           value= {textArea}
           onChange= {e => setTextArea(e.target.value)}>
@@ -64,7 +66,7 @@ const VentanaModal = ({modal, setModal, guardarComentarios}) => {
         <button
           id="btn-submit"
           type="submit"
-          className="p-4 m-4 text-3xl"
+          className="text-4xl font-semibold p-4 m-4 bg-fuchsia-600 rounded-3xl"
           >Enviar comentario
         </button>
       </form>
